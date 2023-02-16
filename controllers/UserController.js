7b9a0d9a131aa252e71c8b72f3b0c44a924be739
andreitoledo <andreiltoledo@hotmail.com>
2023-02-16 15:05:47 -0300

    Atualizando Estatísticas - Usando dataset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -130,11 +130,12 @@ class UserController {
         );
 
     }
-
     
     addLine(dataUser) {
 
         let tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser);
 
         tr.innerHTML = `
             <tr>
@@ -151,6 +152,27 @@ class UserController {
         `;
 
         this.tableEl.appendChild(tr);
+
+        this.updateCount()
+
+    }
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin++;
+        })
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
 
     }
 }
